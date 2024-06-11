76918738d8390255072306a8c8c853bd4125415a
Cristian Kovacs <cris.kovacs14@gmail.com>
2024-06-11 09:11:24 -0400

    Made a small change to the file.

--- src/raylib_game.cpp
+++ src/raylib_game.cpp
@@ -1,21 +1,22 @@
 /*******************************************************************************************
-*
-*   raylib game template
-*
-*   Windows Kill Clone
-*   Raylib Experiment
-*
-*   This game has been created using raylib (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Copyright (c) 2021 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
+ *
+ *   raylib game template
+ *
+ *   Windows Kill Clone
+ *   Raylib Experiment
+ *
+ *   This game has been created using raylib (www.raylib.com)
+ *   raylib is licensed under an unmodified zlib/libpng license (View raylib.h
+ *for details)
+ *
+ *   Copyright (c) 2021 Ramon Santamaria (@raysan5)
+ *
+ ********************************************************************************************/
 
 #include "raylib.h"
 
 #if defined(PLATFORM_WEB)
-    #include <emscripten/emscripten.h>
+#include <emscripten/emscripten.h>
 #endif
 
 //----------------------------------------------------------------------------------
@@ -27,31 +28,30 @@ constexpr int screenHeight{450};
 //----------------------------------------------------------------------------------
 // Main entry point
 //----------------------------------------------------------------------------------
-int main(int argc, char** argv)
-{
-    // Initialization
-    //---------------------------------------------------------
-    InitWindow(screenWidth, screenHeight, "Windows Kill Clone - Experiment");
+int main(int argc, char **argv) {
+  // Initialization
+  //---------------------------------------------------------
+  InitWindow(screenWidth, screenHeight, "Windows Kill Clone - Experiment");
 
 #if defined(PLATFORM_WEB)
-    //emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
+  // emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
 #else
-    SetTargetFPS(60);       // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+  SetTargetFPS(60); // Set our game to run at 60 frames-per-second
+  //--------------------------------------------------------------------------------------
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
-        //UpdateDrawFrame();
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Testing Drawing", 100, 100, 50, BLACK);
-        EndDrawing();
-    }
+  // Main game loop
+  while (!WindowShouldClose()) // Detect window close button or ESC key
+  {
+    // UpdateDrawFrame();
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    DrawText("Testing Drawing", 100, 100, 50, BLACK);
+    EndDrawing();
+  }
 #endif
 
-    CloseWindow();          // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+  CloseWindow(); // Close window and OpenGL context
+  //--------------------------------------------------------------------------------------
 
-    return 0;
+  return 0;
 }
